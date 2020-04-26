1e45438354ca73f3c123e2f043b8cf6c84055f67
dengnici <1119871893@qq.com>
2020-04-26 10:06:35 +0800

    增加多表查询

--- check/views.py
+++ check/views.py
@@ -11,35 +11,47 @@ class get_excel(View):
         path = os.getcwd()+"/check/excel" #后去excel文件夹的绝对路径
         file_list = os.listdir(path) #获取excel文件夹下所有的文件名
         position = []
+        filenames = []
+        info_list = []
+        final_name = []
         for index in range(len(file_list)):
             filesuffix = os.path.splitext(file_list[index])
+            filenames.append(filesuffix[0])
             filesuffix = filesuffix[1]
             if filesuffix == ".xlsx" or filesuffix == ".xls": #仅支持xls和xlsx后缀的excel表格，csv不支持
                 position.append(index)
         for index in position: #循环遍历所有的文件
             file_name = file_list[index]
+            headname = str(filenames[index])
             file_name = "./check/excel/"+file_name
             df = pd.read_excel(file_name,encoding = 'gb18030') #读取excel表格文件中的数据
             df = df.fillna(0) #缺失值用0来填补
             info = df[(df['姓名'] == name) & (df['查询码'] == num)] #查询信息
-            if info.empty == False: #判断是否查询到 
-                break
-        if info.empty == True: #若查询不到则返回0
+            if info.empty == True: #判断是否查询到 
+                continue
+            else:
+                final_name.append(headname)
+                info_list.append(info)
+        if len(info_list) != 0: #输出到'excel.html'当中
+            result = list()
+            for info,fp in zip(info_list,final_name):
+                data = info.values[:,:]
+                excel_data = []
+                li = info.columns.tolist() 
+                excel_data.append(li)
+                for line in data:
+                    ls = []
+                    for j in line:
+                        ls.append(j)
+                    excel_data.append(ls)
+                    context = {
+                        'filename' : fp,
+                        'excel_data' : excel_data
+                    }
+                    result.append(context)
+            return result
+        else:
             return 0
-        else: #输出到'excel.html'当中
-            data = info.values[:,:]
-            excel_data = []
-            li = info.columns.tolist() 
-            excel_data.append(li)
-            for line in data:
-                ls = []
-                for j in line:
-                    ls.append(j)
-                excel_data.append(ls)
-                context = {
-                    'excel_data' : excel_data,
-                }
-                return context
             
     def get(self,request):
         return render(request,'login.html')
@@ -69,7 +81,7 @@ class get_excel(View):
         if info == 0:
             return HttpResponse(alert)
         else:
-            return render(request,self.template_name,context = info) 
+            return render(request,self.template_name,context = {'info' : info}) 
 
 def open_index(request):
     teacher = models.teacher.objects.all()
